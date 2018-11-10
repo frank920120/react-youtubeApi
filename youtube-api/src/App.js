feef94d9fbeee4e6806a4b072714a9c4952739a4
Frank Fang <frankfang2014@hotmail.com>
2018-11-09 16:26:58 -0800

    fetch data using youtube api search

--- youtube-api/src/App.js
+++ youtube-api/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import YTSearch from 'youtube-api-search';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      apiKey :'AIzaSyAtqU_m_lRYnxm8-fp-YUabzq9HLDhPgg0',
+      videos : []
+
+    }
+    this.videoSearch('KDA');
+  }
+  
+  videoSearch(term){
+    YTSearch({
+        key:this.state.apiKey,
+        term:term
+    },(videos)=>{
+        
+       this.setState({
+        videos,
+       },()=>{
+
+        console.log(this.state.videos);
+       });
+    })
+}
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        
       </div>
     );
   }
