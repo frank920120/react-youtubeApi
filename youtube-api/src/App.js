b80ee023375cd8f54332b6ce4722f3d7ec2de660
Frank Fang <frankfang2014@hotmail.com>
2018-11-15 15:48:37 -0800

    update video list

--- youtube-api/src/App.js
+++ youtube-api/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       apiKey :'AIzaSyAtqU_m_lRYnxm8-fp-YUabzq9HLDhPgg0',
-      videos : []
+      videos : [],
+      selectedVideo : null
 
     }
     this.videoSearch('KDA');
   }
-  
+
   videoSearch(term){
     YTSearch({
         key:this.state.apiKey,
@@ -20,6 +22,7 @@ class App extends Component {
         
        this.setState({
         videos,
+        selectedVideo:videos[0]
        },()=>{
 
         console.log(this.state.videos);
@@ -31,7 +34,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+        videos={this.state.videos}
+        />
       </div>
     );
   }
